bf420a824ab54b072826b060d179578a8db28362
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-05-26 17:53:10 +0530

    while and do-while loop

---
+++ 05_iteration.js/02_whileLoop.js
@@ -0,0 +1,32 @@
+/***************************** while Loop ***************************/
+
+
+let index = 0
+while (index <= 20) {
+    console.log(`Value of index ${index}`)
+    index = index + 2    
+}
+
+// loop over Array
+
+let fruits = ["apple", "mango", "orange", "pineapple"]
+
+let i = 0
+while (i < fruits.length) {
+    console.log(`Name of fruit is ${fruits[i]}`)
+    i = i + 1    
+}
+
+
+/********************************* Do-while Loop ************************************/
+
+let marks = 11
+
+do {
+    console.log(`My marks is ${marks}`)
+    marks ++
+
+} while (marks <= 10);
+
+
+
